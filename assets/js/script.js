fa8e2bce7a945348c8fdca389696b7f1903e5049
russe433 <russe433@msu.edu>
2022-05-18 16:36:59 -0400

    added edamam API

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ const searchBtn = document.getElementById("search-btn");
 const recipeList = document.getElementById("recipes");
 const recipeView = document.querySelector("recipe-view");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
-
+var queryIngredient = "#search-btn";
 // Event Listeners
 searchBtn.addEventListener("click", getRecipeList);
 
@@ -32,3 +32,12 @@ function getRecipeList() {
     });
 }
 // Meal DB URL: www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+function getRecipes() {
+  var requestURL = 'https://api.edamam.com/api/food-database/v2/parser?app_id=a7e34d30&app_key=2b1b11a09e2d52c041b4c2fbde20f9cf&ingr='+query+'&nutrition-type-cooking';
+  let searchInputText = document.getElementById("search-input").value.trim();
+  fetch(requestURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
